101bc071decfd77bfb31e52f178cb2e8d678b101
Olav Schettler <olav.schettler@dw.com>
2020-10-13 17:50:36 +0200

    Linked to knowfox

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -40,6 +40,15 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <!-- Knowfox -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Konwfox') }}
+                        </div>
+
+                        <x-jet-dropdown-link href="{{ route('home') }}">
+                            {{ __('Concepts') }}
+                        </x-jet-dropdown-link>
+
                         <!-- Account Management -->
                         <div class="block px-4 py-2 text-xs text-gray-400">
                             {{ __('Manage Account') }}
